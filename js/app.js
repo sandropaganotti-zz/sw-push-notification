af03f921057fa5d067002ba9ddd389d0cf9c071b
Sandro Paganotti <sandro.paganotti@gmail.com>
2015-05-31 14:55:51 +0000

    pushing a demo to gh-pages

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function(){
 	if ('serviceWorker' in navigator) {
-	    navigator.serviceWorker.register('/push-notification-sw.js', {scope: '/'});
+	    navigator.serviceWorker.register('./push-notification-sw.js', {scope: './'});
 	    new pushNotification({
 	   		API_KEY: 'AIzaSyD8fZGjGrC4GDVsE33yv4sCcD7AYpGEZ_o'
 	    });
